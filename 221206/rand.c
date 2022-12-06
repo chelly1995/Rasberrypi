cddafc4477f152c667ea0af4bb01d9abba40cbf2
chelly1995 <chelly94@naver.com>
2022-12-05 23:15:00 -1000

    221206

---
+++ 221206/rand.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+//	srand(10);
+
+	srand((unsigned int)time(NULL));
+	printf("nansu : %d\n",rand());
+	printf("nansu : %d\n",rand());
+	printf("nansu : %d\n",rand());
+	printf("nansu : %d\n",rand());
+	printf("nansu : %d\n",rand());
+	
+
+	return 0;
+
+}
